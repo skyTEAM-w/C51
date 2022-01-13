81873b43bce3785fab9db63a6878914568724533
Wuchengpei_Sky <w2563070231@outlook.com>
2022-01-13 22:52:08 +0800

    LEDMODE ONE

--- LEDKey/main1.c
+++ LEDKey/main1.c
@@ -2,28 +2,39 @@
 #include <INTRINS.H>
 #include "BasicalFunc.h"
 
-unsigned char KeyNum;
+unsigned char KeyNum, LedMode = 0;
 
 void main()
 {
     P0 = 0xFE;
-    // Timer0Init();
+    Timer0_Init();
     while (1)
     {
         KeyNum = Key();
-        
+        if(KeyNum == 1)
+        {
+            LedMode++;
+            LedMode = LedMode % 2;
+        }
     }
 }
 
 void Timer0_Routine() interrupt 1
 {
     static unsigned int T0_Count;
-    TH0 = 0x18;
-    TL0 = 0xFC;
+    TL0 = 0x18;
+    TH0 = 0xFC;
     T0_Count++;
     if (T0_Count >= 500)
     {
         T0_Count = 0;
-        if()
+        if(LedMode == 0)
+        {
+            P0 = _crol_(P0, 1);
+        }
+        if(LedMode == 1)
+        {
+            P0 = _cror_(P0, 1);
+        }
     }
 }
